79bfc394a4d8e8a0f88a47fc96b2eed499de3880
MRuffel <47765411+MRuffel@users.noreply.github.com>
2019-02-20 15:42:18 -0500

    Creating the collectable Nodes

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    #region Stats Variable
+
+    int m_life;
+    int m_score;
+    int m_highScore;
+
+    #endregion
+    #region Control Variable
+
+    [SerializeField]
+    Rigidbody m_player;
+
+    [SerializeField]
+    float m_force;
+
+    float m_horizontalMovement;
+    float m_verticalMovement;
+    #endregion
+
+    #region Singleton
+    //Instace
+    public static PlayerController m_instance { get; private set; }
+
+    //Manage the singleton lifecycle
+    protected virtual void Awake()
+    {
+        if (!m_instance)
+        {
+            m_instance = this;
+        }
+    }
+            #endregion
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+       
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Control();
+    }
+
+
+    void Control() {
+
+        m_horizontalMovement = Input.GetAxis("Horizontal");
+        m_verticalMovement = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(m_horizontalMovement, 0f, m_verticalMovement);
+
+
+        m_player.AddForce(movement * m_force * Time.deltaTime);
+
+    }
+
+}
