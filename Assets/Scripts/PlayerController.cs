4267ceb875cdc2e3ae419357274ef59bb3d58c67
MRuffel <47765411+MRuffel@users.noreply.github.com>
2019-02-20 14:25:52 -0500

    Player Movement done but need some polishing. Starting to collect the node

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,6 +4,25 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    #region Stats Variable
+
+    int m_life;
+    int m_score;
+    int m_highScore;
+
+    #endregion
+    #region Control Variable
+
+    [SerializeField]
+    Rigidbody m_player;
+
+    [SerializeField]
+    float m_force;
+
+    float m_horizontalMovement;
+    float m_verticalMovement;
+#endregion
+
     // Start is called before the first frame update
     void Start()
     {
@@ -11,8 +30,22 @@ public class PlayerController : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        Control();
     }
+
+
+    void Control() {
+
+        m_horizontalMovement = Input.GetAxis("Horizontal");
+        m_verticalMovement = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(m_horizontalMovement, 0f, m_verticalMovement);
+
+
+        m_player.AddForce(movement * m_force * Time.deltaTime);
+
+    }
+
 }
