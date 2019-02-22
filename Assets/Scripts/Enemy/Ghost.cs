111f34c68746fe4c2bf60dc49c807db9ce71fbbc
MRuffel <47765411+MRuffel@users.noreply.github.com>
2019-02-21 20:31:29 -0500

    Starting the script for the AI

---
+++ Assets/Scripts/Enemy/Ghost.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ghost : MonoBehaviour
+{
+    #region Variable
+    Transform m_target;
+
+    Na
+    #endregion
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/Interactable/Collectables.cs
+++ Assets/Scripts/Interactable/Collectables.cs
@@ -33,7 +33,7 @@ public class Collectables : MonoBehaviour
         if(other.tag == m_tagName && m_specialActive) {
 
             //Active the blue mode of the ghost
-            //debug.log("in");
+            Debug.Log("in");
         }
 
     }
